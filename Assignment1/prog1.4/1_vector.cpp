0da10e9df418b6021f5ee8aa93c5f517addf1a82
= <=>
2014-09-21 12:40:21 +0530

    vectors and iterators test

--- Assignment1/prog1.4/1_vector.cpp
+++ Assignment1/prog1.4/1_vector.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(){
-	cout << "Hello World";
+	vector<int> v(10,0);
+	for(vector<int>::iterator i=v.begin();i!=v.end();i++){
+		(*i)++;
+		cout << "5";
+	}
+	for(vector<int>::iterator i=v.begin();i!=v.end();i++){
+                cout << *i << "\n";
+        }
+	cout << "Hello World" << v.size();
 	return 0;
 }
