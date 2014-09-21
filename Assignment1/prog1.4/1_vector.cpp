63febb99817166df380d0d98161512c794693406
= <=>
2014-09-21 13:36:10 +0530

    Insertion and deletion in vector

--- Assignment1/prog1.4/1_vector.cpp
+++ Assignment1/prog1.4/1_vector.cpp
@@ -4,7 +4,7 @@ using namespace std;
 //void insertElements(vector<int>*,int,int);
 void insertElements(vector<int> &v,int n,int times)
 {
-        for(int i=0;i<times;i++){
+	for(int i=0;i<times;i++){
                 for(int j=0;j<n;j++){
                         v.push_back(j);
                 }
@@ -12,18 +12,25 @@ void insertElements(vector<int> &v,int n,int times)
 }
 void deleteElements(vector<int> &v,int n)
 {
-	for(vector<int>::iterator it = v.begin();it!=v.begin()+n;it++){
-		erase(it);	
+	for(int i=0;i<n;i++){
+		v.erase(v.begin());	
+	}
+}
+void displayElements(vector<int> &v){
+	for(vector<int>::iterator it=v.begin();it!=v.end();it++){
+		cout << *it << "\t";
 	}
 }
 int main()
 {
 	vector<int> v;
+	cout << "\nInserting Elements\n";
 	insertElements(v,10,1);
+	cout << "Display Inserted Elements\n";
+	displayElements(v);
+	cout << "\nDeleting Elements\n";
 	deleteElements(v,5);
-	for(vector<int>::iterator it=v.begin();it!=v.end();it++){
-                cout << *it << "\n";
-        }
-	cout << "Hello World" << v.size();
+	cout << "Display Elements after Deletion\n";
+	displayElements(v);
 	return 0;
 }
