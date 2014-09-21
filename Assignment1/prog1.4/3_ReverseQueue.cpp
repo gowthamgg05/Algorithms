4129bf63c827bb3cec75b256cbd7f223e545d954
= <=>
2014-09-21 21:28:25 +0530

    reverse working - swap as a separate function

--- Assignment1/prog1.4/3_ReverseQueue.cpp
+++ Assignment1/prog1.4/3_ReverseQueue.cpp
@@ -1,7 +1,44 @@
 #include<iostream>
 #include<queue>
 using namespace std;
+void swap(int *first,int *last){
+	int temp;
+	temp = *first;
+	*first = *last;
+	*last = temp;
+}
+void reverseQueue(queue<int> &q){
+	int *first = &q.front();
+	int *last = &q.back();
+	while(first<last){
+		swap(first,last);
+		first++;
+		last--;
+	}
+}
 int main()
 {
+	queue<int> q;
+	q.push(10);
+	q.push(20);
+	q.push(33);
+	
+                int *first = &q.front();
+                int *last = &q.back();
+                while(first<=last){
+                        cout << *first << "\n";
+                        first++;
+                }
+        
+
+	reverseQueue(q);
+
+		int *f = &q.front();
+        	int *l = &q.back();
+        	while(f<=l){
+                	cout << *f << "\n";
+			f++;
+        	}
+	
 	cout << "HEllo";
 }
