2c4f334a605dc2402651586b5c22e8bc9db73773
= <=>
2014-09-21 22:50:17 +0530

    file read successful

--- Assignment1/prog1.4/4_T2DA.cpp
+++ Assignment1/prog1.4/4_T2DA.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
+#include<fstream>
+#include<string>
 using namespace std;
-int main ()
+int main(int argc,char **argv)
 {
-	cout << "hello";
+	int noOfProperties;
+	ifstream fp(argv[1]);
+	/*while(getline(fp,s)){
+		cout << s;
+	}*/
+	/*while(fp >> s){
+		cout << s;
+	}*/
+	if(fp)
+	{
+                fp >> noOfProperties;
+				
+	}else{
+		cout << "\nFile not found at " << argv[1] << "\nPlease mention the full path of the file";
+		cout << "\nSpecify the path in double quotes";
+		cout << "\nMake sure the file path doesn't have white space character(s)\n";
+	}
+	cout << noOfProperties;
+	fp.close();
 }
