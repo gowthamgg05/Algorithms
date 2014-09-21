1c96d27864e003e6158728d2f60fac97ce3a1abb
= <=>
2014-09-21 17:19:37 +0530

    Final - Email structure, spam removal, modified email structure and displaying the last recipient

--- Assignment1/prog1.4/2_vector_email.cpp
+++ Assignment1/prog1.4/2_vector_email.cpp
@@ -19,8 +19,7 @@ struct EmailMsg createEmailStructure(string to,string from,string subject,string
 void displayEmailStructure(vector<struct EmailMsg> &v){
 	cout << "EMAILS\n";
 	for(vector<struct EmailMsg>::iterator it=v.begin();it!=v.end();it++){
-                cout << "To:" << it->to << "\n";                //'*(it).to' to access the structure value is not working       
-                cout << "From:" << it->from << "\n";
+		cout << "From:" << it->from << "\n"; //'*(it).to' to access the structure value is not working
                 cout << "subject:" << it->subject << "\n";
                 cout << "message:" << it->message << "\n";
                 cout << "data:" << it->data << "\n";
@@ -43,6 +42,36 @@ void removeSpam(vector<struct EmailMsg> &v){
 	}
 	cout << "\n" << count << " spam(s) removed\n";
 }
+//Structure of EmailMsg to hold multiple recipients
+struct ModifiedEmailMsg{
+        vector<string> to;
+        string from;
+        string subject;
+        string message;
+        int data;
+        int time;
+};
+//Initialising the 'ModifiedEmailMsg' structure
+struct ModifiedEmailMsg createModifiedEmailStructure(vector<string> &to,string from,string subject,string message,int data,int time){
+	struct ModifiedEmailMsg e = {to,from,subject,message,data,time};
+	return e;
+}
+//Display the EmailMsgs in a vector v
+void displayModifiedEmailStructure(vector<struct ModifiedEmailMsg> &v){
+        cout << "MODIFIED EMAIL STRUCTURE - EMAILS\n";
+        for(vector<struct ModifiedEmailMsg>::iterator it=v.begin();it!=v.end();it++){
+		cout << "To:";
+		int noOfRecipients=(it->to).size();
+		for(int i=0;i<noOfRecipients;i++)
+			cout << (it->to)[i] << " ; ";
+		cout << "\nFrom:" << it->from << "\n";
+                cout << "subject:" << it->subject << "\n";
+                cout << "message:" << it->message << "\n";
+                cout << "data:" << it->data << "\n";
+                cout << "time:" << it->time << "\n";
+		cout << "LAST RECIPIENT  :  " << (it->to)[(it->to).size()-1] << "\n"; //Accessing last recipient
+        }
+}
 int main()
 {
 	//Vector of 'EmailMsg' structure
@@ -50,7 +79,7 @@ int main()
 	//Initialize the structure
 	struct EmailMsg e;
 	e = createEmailStructure("Gowthamgg05@hotmail.com","gowtham.radhakrishna@gmail.com","Hiii","Helllo",2,24);
-	//Push it to the vector
+	//Push it to the vector v
 	v.push_back(e);
 	e = createEmailStructure("Gowtham","gowtham.radhakrishna@solitontech.com","SPAM This is not a spam","Helllo",2,24);
 	v.push_back(e);
@@ -58,6 +87,20 @@ int main()
         v.push_back(e);
 	//Display EmailMsgs
 	displayEmailStructure(v);
+	//Remove Spam
 	removeSpam(v);
 	displayEmailStructure(v);
+
+	//vector of 'ModifiedEmailMsg' structure
+	vector<struct ModifiedEmailMsg> m;
+	//Initialize the structure for 'ModifiedEmailMsg'
+	struct ModifiedEmailMsg me;
+	vector<string> recipients;
+	recipients.push_back("abc@gmail.com");
+	recipients.push_back("xyz@gmail.com");
+	recipients.push_back("gowthamgg05@gmail.com");
+	me = createModifiedEmailStructure(recipients,"mogith@gmail.com","Have a good day","Good Morning",3,12);
+	//Push it to the vector m
+	m.push_back(me);
+	displayModifiedEmailStructure(m);
 }
