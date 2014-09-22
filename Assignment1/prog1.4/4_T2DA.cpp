d7949bc655b5cde248009c62bc130b19ba5f4db4
= <=>
2014-09-22 20:18:19 +0530

    Area of triangle working - Vector repetition print fix

--- Assignment1/prog1.4/4_T2DA.cpp
+++ Assignment1/prog1.4/4_T2DA.cpp
@@ -23,13 +23,14 @@ int main(int argc,char **argv)
 	if(fp)
 	{
                 fp >> noOfProperties;
-		vector<Property> v(noOfProperties);
+		vector<Property> v;
 		for(int i=0;i<noOfProperties;i++){
 			fp >> coOrdinates;
 			if(coOrdinates==1){
 				fp >> cost;
 				fp >> radius;
 				Property p = {"Circle",cost*3.14*radius*radius};
+				cout << "circle";
 				v.push_back(p);
 				continue;	
 			}
@@ -39,12 +40,13 @@ int main(int argc,char **argv)
 				fp >> d[0].y;
 				fp >> d[1].x;
 				fp >> d[1].y;
-				float length = (d[0].x > d[1].x)?(d[0].x-d[1].x):(d[1].x-d[0].x);
-				float breadth = (d[0].y > d[1].y)?(d[0].y-d[1].y):(d[1].y-d[0].y); 
-				length = abs(d[0].x-d[1].x);
-				breadth = abs(d[0].y-d[1].y);
+				//float length = (d[0].x > d[1].x)?(d[0].x-d[1].x):(d[1].x-d[0].x);
+				//float breadth = (d[0].y > d[1].y)?(d[0].y-d[1].y):(d[1].y-d[0].y); 
+				float length = abs(d[0].x-d[1].x);
+				float breadth = abs(d[0].y-d[1].y);
 				Property p = {"Rectangle",cost*length*breadth};
 				v.push_back(p);	
+				cout << "Rectangle";
 				continue;
 			}
 			if(coOrdinates==3){
@@ -58,13 +60,15 @@ int main(int argc,char **argv)
 				float area = abs(d[0].x*(d[1].y-d[2].y)+d[1].x*(d[2].y-d[0].y)+d[2].x*(d[0].y-d[1].y))/2;
 				Property p = {"Triangle",cost*area};
 				v.push_back(p);
+				cout << "Triangle";
 				continue;
 			}
 		}
 		for(vector<Property>::iterator it=v.begin();it!=v.end();it++){
-               	        cout << "Property Type" << it->propertyName << "\n";
-                       	cout << "Price of the property" << it->price << "\n\n";
-        	}		
+               	        cout << "Property Type : " << it->propertyName << "\n";
+                       	cout << "Price of the property : " << it->price << "\n\n";
+        	}
+		cout << "Asdsad" << v.size();		
 	}else{
 		cout << "\nFile not found at " << argv[1] << "\nPlease mention the full path of the file";
 		cout << "\nSpecify the path in double quotes";
