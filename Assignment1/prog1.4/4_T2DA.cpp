43cd1c5852d4ba6e892d925413a14f4e6e1b835a
= <=>
2014-09-22 20:01:59 +0530

    Area of triangle working - removed area function

--- Assignment1/prog1.4/4_T2DA.cpp
+++ Assignment1/prog1.4/4_T2DA.cpp
@@ -14,17 +14,6 @@ typedef struct property{
 	float price;
 } Property;
 					      					          
-float computeAreaOfTriangleWithVertices(float ax,float ay,float bx,float by,float cx,float cy)
-{
-    /*float A = sqrt((double)(bx-ax) * (bx-ax) + (by-ay) * (by-ay));
-    float B = sqrt((double)(bx-cx) * (bx-cx) + (by-cy) * (by-cy));
-    float C = sqrt((double)(ax-cx) * (ax-cx) + (ay-cy) * (ay-cy));
-    float s = (A + B + C) / 2; 
-    float area = sqrt( s * (s-A) * (s-B) * (s-C));
-    return area;*/
-    return abs(ax*(by-cy)+bx*(cy-ay)+cx*(ay-by))/2;
-}
-
 int main(int argc,char **argv)
 {
 	int noOfProperties;
@@ -52,6 +41,8 @@ int main(int argc,char **argv)
 				fp >> d[1].y;
 				float length = (d[0].x > d[1].x)?(d[0].x-d[1].x):(d[1].x-d[0].x);
 				float breadth = (d[0].y > d[1].y)?(d[0].y-d[1].y):(d[1].y-d[0].y); 
+				length = abs(d[0].x-d[1].x);
+				breadth = abs(d[0].y-d[1].y);
 				Property p = {"Rectangle",cost*length*breadth};
 				v.push_back(p);	
 				continue;
@@ -64,23 +55,16 @@ int main(int argc,char **argv)
                                 fp >> d[1].y;
 				fp >> d[2].x;
 				fp >> d[2].y;
-				cout << d[0].x << "\t" << d[0].y << "\n";
-				cout << d[1].x << "\t" << d[1].y << "\n";
-				cout << d[2].x << "\t" << d[2].y << "\n";
-				float area = computeAreaOfTriangleWithVertices(d[0].x,d[0].y,d[1].x,d[1].y,d[2].x,d[2].y);
-				//abs(ax*(by-cy)+bx*(cy-ay)+cx*(ay-by))/2;
-				area = abs(d[0].x*(d[1].y-d[2].y)+d[1].x*(d[2].y-d[0].y)+d[2].x*(d[0].y-d[1].y))/2;
-				cout << area;
-				Property p = {"Rectangle",cost*area};
+				float area = abs(d[0].x*(d[1].y-d[2].y)+d[1].x*(d[2].y-d[0].y)+d[2].x*(d[0].y-d[1].y))/2;
+				Property p = {"Triangle",cost*area};
 				v.push_back(p);
-				//cout << "\nTp" << p.price;
 				continue;
 			}
 		}
-		/*for(vector<Property>::iterator it=v.begin();it!=v.end();it++){
+		for(vector<Property>::iterator it=v.begin();it!=v.end();it++){
                	        cout << "Property Type" << it->propertyName << "\n";
                        	cout << "Price of the property" << it->price << "\n\n";
-        	}*/		
+        	}		
 	}else{
 		cout << "\nFile not found at " << argv[1] << "\nPlease mention the full path of the file";
 		cout << "\nSpecify the path in double quotes";
