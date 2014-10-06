d630e850056a6efaf2a2ca3504848b6f2b53fc93
= <=>
2014-10-06 20:04:22 +0530

    Finding Mean - completed

--- Assignment3/FindMean.cpp
+++ Assignment3/FindMean.cpp
@@ -1,29 +1,55 @@
-nclude <cmath>
-#include <cstdio>
 #include <vector>
 #include <iostream>
-#include <algorithm>
+#include<fstream>
 using namespace std;
-
-
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    int a[]={7,1,2,4,6,5,3};
-    int x = a[4];
-    int i = 0;
-    for(int j=i+1;j<7;j++){
-        if(a[j]<x){
-            i+=1;
-            int temp;
-            temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
-        }
-    }
-    for(int k=0;k<7;k++){
-        cout << a[k] << endl;
-    }
-    cout << "pivot" << i;
-    return a[i];
+//Swap ith and jth index
+void swap(vector<int> &a,int i,int j){
+	int temp;
+    	temp = a[i];
+    	a[i] = a[j];
+    	a[j] = temp;
 }
 
+//Begining of the vector is chosen as pivot element. Returns the pivot index
+int partition(vector<int> &a,int p,int q){
+    	int x = a[p];
+    	int i = p;
+    	for(int j=p+1;j<=q;j++){
+        	    if(a[j]<=x){
+                	i++;
+                	swap(a,i,j);
+            	}
+    	}
+    	swap(a,p,i);
+    	return i;
+}
+
+//Recursively search whether the pivot index is in the middle of the vector or not
+void findMedian(vector<int> &a,int p,int q,int k){
+	int index = partition(a,p,q);
+    	if(index == k/2)
+        	cout << a[index];
+    	else if(index > k/2)
+        	findMedian(a,p,index-1,k);
+    	else if(index < k/2)
+        	findMedian(a,index+1,q,k);
+}
+
+int main(int argc,char **argv) {
+    	int n,k;
+	ifstream fp(argv[1]);
+	if(fp){
+		fp >> n;
+		vector<int> v;
+		int item;
+		for(int i=0;i<n;i++){
+			fp >> item;
+			v.push_back(item);
+		}
+		findMedian(v,0,n-1,n);
+	}
+	else
+	{
+		cout << "File not found";
+	}
+}
