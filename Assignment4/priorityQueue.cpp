f44d99c840349c87adf9a317a19060cd92a806a0
gowtham <gowthamgg05@gmail.com>
2014-10-19 19:54:16 +0530

    documented completed

--- Assignment4/priorityQueue.cpp
+++ Assignment4/priorityQueue.cpp
@@ -2,63 +2,149 @@
 #include<vector>
 using namespace std;
 
+// Priority Queue
+template <class T>
 class PQueue
 { 
 	public:
-		bool impType;
-		vector<int> queue;
-		PQueue(bool impTyp){
+		bool impType;	// Whether to use unsorted array or binary heap implementation
+		vector<T> queue;	
+		PQueue(bool impTyp){	// Constructor for implementation type initialization	
 			impType = impTyp;
 		};
 		~PQueue(){};
-		int size();
-		bool IsEmpty();
-		void Enqueue(int new_element);
-		int RemoveMin();
+		int size();	// Returns size of Priority queue
+		bool IsEmpty();		// Returns true if Priority queue is empty
+		void Enqueue(T new_element);	// Adds element to the end of the queue
+		T RemoveMin();	// Removes less priority element - In this case, minimum value is the lowest priority element
+		void display();	
 };
-int PQueue::size(){
+
+template <class T>
+int PQueue<T>::size(){
 	return queue.size();
 }
-bool PQueue::IsEmpty(){
+template <class T>
+bool PQueue<T>::IsEmpty(){
 	return (size()==0);
 }
-void PQueue::Enqueue(int new_element){
+template <class T>
+void PQueue<T>::Enqueue(T new_element){
 	queue.push_back(new_element);
 }
-int  PQueue::RemoveMin(){
+
+int leftChild(int i){
+        return i*2+1;
+}
+//HeapSort - Finding right child
+int rightChild(int i){
+        return i*2+2;
+}
+//HeapSort - Finding parent
+int parent(int i){
+        return (i-1)/2;
+}
+//HeapSort - Swap two template elements
+template <class T>
+void swap(vector<T> &v,int firstIndex,int secondIndex){
+        T temp;
+        temp = v[firstIndex];
+        v[firstIndex] = v[secondIndex];
+        v[secondIndex] = temp;
+}
+//HeapSort - To maintain the heap property
+template <class T>
+void heapify(vector<T> &v,int i,int heapSize){
+        int l = leftChild(i);
+        int r = rightChild(i);
+        int largest;
+        if(l<=heapSize && v[i]>v[l])
+                largest = l;
+        else
+                largest = i;
+        if(r<=heapSize && v[largest]>v[r])
+                largest = r;
+        if(largest!=i){
+                swap(v,largest,i);
+                heapify(v,largest,heapSize);
+        }
+}
+//HeapSort - Builds a heap from the array by calling Heapify
+template <class T>
+void buildHeap(vector<T> &v){
+        int heapSize = v.size()-1;
+        for(int i=(v.size()-2)/2;i>=0;i--)
+        {
+                heapify(v,i,heapSize);
+        }
+}
+
+// Priority Queue - Remove Minimum
+template <class T>
+T PQueue<T>::RemoveMin(){
 	if(impType==true){
-		vector<int> :: iterator minIndex=queue.begin();
-		for(vector<int>::iterator i=queue.begin();i!=queue.end();i++){
-			if(i==queue.begin())
+		int minIndex=0;
+		for(int i=0;i<size();i++){
+			if(i==0)
 				minIndex=i;
-			if(*i<*minIndex)
+			if(queue[i]<queue[minIndex])
 				minIndex=i;	
 		}
-		int minElement = *minIndex;
-		queue.erase(minIndex);
+		T minElement = queue[minIndex];
+		queue.erase(queue.begin()+minIndex);
+		return minElement;
+	}
+	else
+	{
+		buildHeap(queue);
+        	int heapSize=size()-1;
+		T minElement = queue[0];
+		heapify(queue,0,heapSize);
+		heapSize--;	
+		queue.erase(queue.begin());
 		return minElement;
 	}
 }
-
-int main(){
-	PQueue pqueue(true),pqueue1(false);
-	pqueue.Enqueue(0);
-	pqueue.Enqueue(2);
-	pqueue.Enqueue(3);
-	pqueue.Enqueue(1);
-	pqueue.Enqueue(4);
-	pqueue1.Enqueue(0);
-        pqueue1.Enqueue(2);
-        pqueue1.Enqueue(3);
-        pqueue1.Enqueue(1);
-        pqueue1.Enqueue(4);
-	//cout << pqueue.size();
-	//pqueue.RemoveMin();
-	//cout << pqueue.size();
-	//cout << pqueue.IsEmpty();
-	//cout << "\n";
-	pqueue1.RemoveMin();
-	//heapSort(pqueue1);
-	//display(pqueue1);
-	//cout << pqueue.impType << "\t" << pqueue1.impType;
+template <class T>
+void PQueue<T>::display(){
+	for(int i=0;i<size();i++){
+		cout << queue[i] << "\t";
+	}
 }
+int main(){
+	
+	// unsorted array as priority queue - arg : true
+	PQueue<int> pqueueArray(true);
+	// heap as priority queue - arg : false
+	PQueue<char> pqueueHeap(false);
+	pqueueArray.Enqueue(0);
+	pqueueArray.Enqueue(2);
+	pqueueArray.Enqueue(3);
+	pqueueArray.Enqueue(1);
+	pqueueArray.Enqueue(4);
+	pqueueArray.Enqueue(12);
+	pqueueArray.Enqueue(7);
+	pqueueHeap.Enqueue('g');
+        pqueueHeap.Enqueue('o');
+        pqueueHeap.Enqueue('w');
+        pqueueHeap.Enqueue('t');
+        pqueueHeap.Enqueue('h');
+	pqueueHeap.Enqueue('a');
+	pqueueHeap.Enqueue('m');
+	// Repeat for three iterations
+	for(int i=0;i<3;i++){
+		cout << "\n\nunsorted array : ";
+		pqueueArray.display();
+		cout << "\nbinary heap    : ";
+		pqueueHeap.display();
+		cout << "\nunsorted array : size - " << pqueueArray.size();
+		cout << "\nbinary heap    : size - " << pqueueHeap.size();
+		cout << "\nunsorted array : Remove Min - " << pqueueArray.RemoveMin();
+        	cout << "\nbinary heap    : Remove Min - " << pqueueHeap.RemoveMin();
+		cout << "\nunsorted array : ";
+        	pqueueArray.display();
+        	cout << "\nbinary heap    : ";
+        	pqueueHeap.display();
+	}
+}
+	
